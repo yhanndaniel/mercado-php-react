0ebbef22f4865208da0a82dc6165284dcd86ebc1
Yhann Daniel <yhanndaniel@gmail.com>
2023-07-09 11:33:12 -0300

    use .env

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
+
+use App\Connection\DatabaseConnection;
+use Symfony\Component\Dotenv\Dotenv;
+
+include_once __DIR__.'/vendor/autoload.php';
+
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
+
+(new Dotenv())->load(__DIR__.'/.env');
+
+//var_dump($_ENV);
 
 date_default_timezone_set('America/Sao_Paulo');
 
@@ -9,4 +19,13 @@ $method = $_SERVER['REQUEST_METHOD'];
 
 $path = explode('/', $url);
 
-var_dump($url, $path, $method);
+$db = DatabaseConnection::open();
+$rs = $db->prepare("SELECT x.* FROM db_estacio_library.tb_category x");
+$rs->execute();
+$obj = $rs->fetchAll(PDO::FETCH_ASSOC);
+
+DatabaseConnection::close($db);
+
+echo json_encode($obj);
+
+//var_dump($url, $path, $method);
