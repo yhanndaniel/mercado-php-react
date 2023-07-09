74c5062e7c7290a1b023929963f118f490d0f697
Yhann Daniel <yhanndaniel@gmail.com>
2023-07-09 14:23:44 -0300

    implements inner join e limit

--- src/Database/Select.php
+++ src/Database/Select.php
@@ -4,37 +4,59 @@ namespace App\Database;
 
 class Select
 {
+    private string $table;
     private string $sql;
-    private bool $where = false;
+    private string $order;
+    private string $limit;
+    private array $wheres = [];
+    private array $joins = [];
     private array $binds = [];
 
-    public function query(string $query)
+    public function query(string $table)
     {
-        $this->sql = $query;
+        $this->table = $table;
+        $this->sql = 'SELECT * FROM ' . $this->table;
         return $this;
     }
 
     public function where(string $field, string $operator, mixed $value, ?string $type = null)
     {
-        (!$this->where) ?
-            $this->sql .= " WHERE {$field} {$operator} :{$field} {$type}":
-            $this->sql .= " {$field} {$operator} :{$field}";
+        $this->wheres[] = "{$field} {$operator} :{$field} {$type} ";
 
-        $this->sql = rtrim($this->sql);
         $this->binds[$field] = $value;
-        $this->where = true;
 
+        return $this;
+    }
+
+    public function join(string $SecondaryTable, string $primaryKey, string $foreignKey)
+    {
+        $this->joins[] = " INNER JOIN {$SecondaryTable} ON {$SecondaryTable}.{$foreignKey} = {$this->table}.{$primaryKey}";
         return $this;
     }
 
     public function order(string $field, string $direction = 'ASC')
     {
-        $this->sql .= " ORDER BY {$field} {$direction}";
+        $this->order = " ORDER BY {$field} {$direction}";
         return $this;
+    }
+
+    public function limit(int $limit)
+    {
+        $this->limit = " LIMIT {$limit}";
+        return $this;
+    }
+
+    public function dump()
+    {
+        $this->sql.= (!empty($this->joins)) ? rtrim(implode('', $this->joins)) : '';
+        $this->sql.= (!empty($this->wheres)) ? rtrim(' WHERE '.implode('', $this->wheres)) : '';
+        $this->sql.= $this->order ?? '';
+        $this->sql.= $this->limit ?? '';
     }
 
     public function get()
     {
+        $this->dump();
         return (object)[
             'sql' => $this->sql,
             'binds' => $this->binds
--- tests/Database/SelectTest.php
+++ tests/Database/SelectTest.php
@@ -17,19 +17,19 @@ class SelectTest extends TestCase
     }
     public function test_get_simple_select()
     {
-        $query = $this->select->query('SELECT * FROM users')->get();
+        $query = $this->select->query('users')->get();
         $this->assertEquals('SELECT * FROM users', $query->sql);
     }
 
     public function test_get_select_with_conditional()
     {
-        $query = $this->select->query('SELECT * FROM users')->where('id', '>', 10)->get();
+        $query = $this->select->query('users')->where('id', '>', 10)->get();
         $this->assertEquals('SELECT * FROM users WHERE id > :id', $query->sql);
     }
 
     public function test_get_select_with_more_than_one_conditional()
     {
-        $query = $this->select->query('SELECT * FROM users')
+        $query = $this->select->query('users')
             ->where('id', '>', 10, 'AND')
             ->where('firstName', '=', 'Yhann')
             ->get();
@@ -38,7 +38,7 @@ class SelectTest extends TestCase
 
     public function test_get_select_with_more_than_one_conditional_and_use_type_conditional()
     {
-        $query = $this->select->query('SELECT * FROM users')
+        $query = $this->select->query('users')
             ->where('id', '>', 10, 'OR')
             ->where('firstName', '=', 'Yhann')
             ->get();
@@ -47,7 +47,7 @@ class SelectTest extends TestCase
 
     public function test_get_binds_from_conditional()
     {
-        $query = $this->select->query('SELECT * FROM users')
+        $query = $this->select->query('users')
             ->where('id', '>', 10, 'OR')
             ->where('firstName', '=', 'Yhann')
             ->get();
@@ -56,7 +56,7 @@ class SelectTest extends TestCase
 
     public function test_get_select_with_order_by()
     {
-        $query = $this->select->query('SELECT * FROM users')
+        $query = $this->select->query('users')
             ->order('id', 'DESC')
             ->get();
         $this->assertEquals('SELECT * FROM users ORDER BY id DESC', $query->sql);
@@ -64,11 +64,114 @@ class SelectTest extends TestCase
 
     public function test_get_select_with_conditional_and_order_by_with_different_order()
     {
-        $query = $this->select->query('SELECT * FROM users')
+        $query = $this->select->query('users')
             ->order('id', 'DESC')
             ->where('id', '>', 10)
             ->get();
         $this->assertEquals('SELECT * FROM users WHERE id > :id ORDER BY id DESC', $query->sql);
+    }
+
+    public function test_get_select_with_limit()
+    {
+        $query = $this->select->query('users')
+            ->limit(10)
+            ->get();
+        $this->assertEquals('SELECT * FROM users LIMIT 10', $query->sql);
+    }
+
+    public function test_get_select_with_conditional_and_limit()
+    {
+        $query = $this->select->query('users')
+            ->limit(10)
+            ->where('id', '>', 10)
+            ->get();
+        $this->assertEquals('SELECT * FROM users WHERE id > :id LIMIT 10', $query->sql);
+    }
+
+    public function test_get_select_with_more_than_one_conditional_and_limit()
+    {
+        $query = $this->select->query('users')
+            ->limit(10)
+            ->where('id', '>', 10, 'AND')
+            ->where('firstName', '=', 'Yhann')
+            ->get();
+        $this->assertEquals('SELECT * FROM users WHERE id > :id AND firstName = :firstName LIMIT 10', $query->sql);
+    }
+
+    public function test_get_select_with_more_than_one_conditional_and_limit_and_use_type_conditional()
+    {
+        $query = $this->select->query('users')
+            ->limit(10)
+            ->where('id', '>', 10, 'OR')
+            ->where('firstName', '=', 'Yhann')
+            ->get();
+        $this->assertEquals('SELECT * FROM users WHERE id > :id OR firstName = :firstName LIMIT 10', $query->sql);
+    }
+
+    public function test_get_select_with_more_than_one_conditional_and_limit_and_use_type_conditional_and_order_by()
+    {
+        $query = $this->select->query('users')
+            ->limit(10)
+            ->where('id', '>', 10, 'OR')
+            ->where('firstName', '=', 'Yhann')
+            ->order('id', 'DESC')
+            ->get();
+        $this->assertEquals('SELECT * FROM users WHERE id > :id OR firstName = :firstName ORDER BY id DESC LIMIT 10', $query->sql);
+    }
+
+    public function test_select_with_joins()
+    {
+        $query = $this->select->query('users')
+            ->join('comments', 'id', 'user_id')
+            ->get();
+
+        $this->assertEquals('SELECT * FROM users INNER JOIN comments ON comments.user_id = users.id', $query->sql);
+    }
+
+    public function test_select_with_multiple_joins()
+    {
+        $query = $this->select->query('users')
+            ->join('comments', 'id', 'user_id')
+            ->join('posts', 'id', 'user_id')
+            ->get();
+
+        $this->assertEquals('SELECT * FROM users INNER JOIN comments ON comments.user_id = users.id INNER JOIN posts ON posts.user_id = users.id', $query->sql);
+    }
+
+    public function test_select_with_multiple_joins_and_where()
+    {
+        $query = $this->select->query('users')
+            ->join('comments', 'id', 'user_id')
+            ->join('posts', 'id', 'user_id')
+            ->where('users.id', '>', 10)
+            ->get();
+
+        $this->assertEquals('SELECT * FROM users INNER JOIN comments ON comments.user_id = users.id INNER JOIN posts ON posts.user_id = users.id WHERE users.id > :users.id', $query->sql);
+    }
+
+    public function test_select_with_multiple_joins_and_multiple_where()
+    {
+        $query = $this->select->query('users')
+            ->join('comments', 'id', 'user_id')
+            ->join('posts', 'id', 'user_id')
+            ->where('users.id', '>', 10, 'AND')
+            ->where('posts.id', '>', 10)
+            ->get();
+
+        $this->assertEquals('SELECT * FROM users INNER JOIN comments ON comments.user_id = users.id INNER JOIN posts ON posts.user_id = users.id WHERE users.id > :users.id AND posts.id > :posts.id', $query->sql);
+    }
+
+    public function test_select_with_multiple_joins_and_multiple_where_and_order_by()
+    {
+        $query = $this->select->query('users')
+            ->join('comments', 'id', 'user_id')
+            ->join('posts', 'id', 'user_id')
+            ->where('users.id', '>', 10, 'AND')
+            ->where('posts.id', '>', 10)
+            ->order('users.id', 'DESC')
+            ->get();
+
+        $this->assertEquals('SELECT * FROM users INNER JOIN comments ON comments.user_id = users.id INNER JOIN posts ON posts.user_id = users.id WHERE users.id > :users.id AND posts.id > :posts.id ORDER BY users.id DESC', $query->sql);
     }
 
 }
