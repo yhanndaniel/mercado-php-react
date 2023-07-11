4d3f3921b60176111eeb7b261e11693590c07029
Yhann Daniel <yhanndaniel@gmail.com>
2023-07-10 22:12:39 -0300

    Refectory

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 
-require './bootstrap.php';
+use App\Library\Router;
+use Symfony\Component\Dotenv\Dotenv;
+
+include_once '../vendor/autoload.php';
+
+(new Dotenv())->load('../.env');
+
+date_default_timezone_set('America/Sao_Paulo');
+$router = new Router;
+
 require '../src/Routes/web.php';
