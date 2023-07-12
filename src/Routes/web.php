7ffa60212982559894ff2a42b5aa4ec99ffa07c1
Yhann Daniel <yhanndaniel@gmail.com>
2023-07-12 08:42:54 -0300

    Build React front-end

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,6 +3,8 @@
 try {
     $router->add('/', 'GET', 'HomeController:index');
     $router->add('/admin', 'GET', 'HomeController:index');
+    $router->add('/admin/produtos', 'GET', 'HomeController:index');
+    $router->add('/admin/vendas', 'GET', 'HomeController:index');
 } catch (Exception $e) {
     var_dump($e->getMessage() . ' ' . $e->getFile() . ' ' . $e->getLine());
 }
